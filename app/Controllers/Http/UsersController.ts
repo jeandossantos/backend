06323e31967af1ad575e4271839815073de57fe0
Jean F. dos Santos <jeanddg@hotmail.com>
2022-09-28 14:05:45 -0300

    add validation to create user controller

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,10 +1,23 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import User from "App/Models/User";
 
+import { schema, rules } from "@ioc:Adonis/Core/Validator";
+
 export default class UsersController {
   async create({ request }: HttpContextContract) {
-    const { name, initials, email, password, confirmPassword } = request.body();
+    const newUserSchema = schema.create({
+      name: schema.string(),
+      email: schema.string({}, [rules.email()]),
+      initials: schema.string(),
+      password: schema.string({}, [
+        rules.minLength(5),
+        rules.confirmed("confirmPassword"),
+      ]),
+    });
 
+    const payload = await request.validate({ schema: newUserSchema });
+
+    const { name, initials, email, password } = payload;
     const user = await User.create({
       name,
       email,
