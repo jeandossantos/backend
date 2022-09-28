10f2e6b46209aeca202b0bb69cc54e2c37eeb51e
Jean F. dos Santos <jeanddg@hotmail.com>
2022-09-28 14:32:50 -0300

    creating update user method and validations

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -8,7 +8,7 @@ export default class UsersController {
     const newUserSchema = schema.create({
       name: schema.string(),
       email: schema.string({}, [rules.email()]),
-      initials: schema.string(),
+      initials: schema.string({}, [rules.minLength(2), rules.maxLength(2)]),
       password: schema.string({}, [
         rules.minLength(5),
         rules.confirmed("confirmPassword"),
@@ -18,12 +18,35 @@ export default class UsersController {
     const payload = await request.validate({ schema: newUserSchema });
 
     const { name, initials, email, password } = payload;
+
     const user = await User.create({
       name,
       email,
       initials,
       password,
     });
+
+    return user;
+  }
+
+  async update({ request }: HttpContextContract) {
+    const newUserSchema = schema.create({
+      name: schema.string(),
+      initials: schema.string({}, [rules.minLength(2), rules.maxLength(2)]),
+    });
+
+    const userId = request.param("id");
+
+    const payload = await request.validate({ schema: newUserSchema });
+
+    const { name, initials } = payload;
+
+    const user = await User.findOrFail(userId);
+
+    user.name = name;
+    user.initials = initials;
+
+    await user.save();
 
     return user;
   }
--- start/routes.ts
+++ start/routes.ts
@@ -21,3 +21,4 @@
 import Route from "@ioc:Adonis/Core/Route";
 
 Route.post("/users", "UsersController.create");
+Route.put("/users/:id", "UsersController.update");
