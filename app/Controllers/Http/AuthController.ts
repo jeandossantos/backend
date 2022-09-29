184c0c692fce1ad8f77c177653e6e43701a29cee
Jean F. dos Santos <jeanddg@hotmail.com>
2022-09-29 17:56:12 -0300

    implementation login with email and password

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,15 @@
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+
+export default class AuthController {
+  async login({ request, response, auth }: HttpContextContract) {
+    const email = request.input("email");
+    const password = request.input("password");
+
+    try {
+      const token = await auth.use("api").attempt(email, password);
+      return token;
+    } catch {
+      return response.unauthorized("Invalid credentials");
+    }
+  }
+}
