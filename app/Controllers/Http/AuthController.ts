0b70e14c1967f140c23b04ce1258db039c842a8f
Jean F. dos Santos <jeanddg@hotmail.com>
2022-09-29 18:42:13 -0300

    adding user info into return

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,15 +1,28 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import User from "App/Models/User";
+import Hash from "@ioc:Adonis/Core/Hash";
 
 export default class AuthController {
   async login({ request, response, auth }: HttpContextContract) {
     const email = request.input("email");
     const password = request.input("password");
 
-    try {
-      const token = await auth.use("api").attempt(email, password);
-      return token;
-    } catch {
-      return response.unauthorized("Invalid credentials");
+    const user = await User.query().where("email", email).firstOrFail();
+
+    if (!(await Hash.verify(user.password, password))) {
+      return response.unauthorized("Invalid e-mail/password");
     }
+
+    const token = await auth.use("api").attempt(email, password);
+
+    return { user, token };
+  }
+
+  async logout({ auth }: HttpContextContract) {
+    await auth.use("api").revoke();
+
+    return {
+      revoked: true,
+    };
   }
 }
